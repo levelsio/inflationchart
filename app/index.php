799653b5c28d5f7dd64979ab5df9612508f32bbc
levelsio <7150848+levelsio@users.noreply.github.com>
2021-03-28 06:03:05 +0100

    x

--- app/index.php
+++ app/index.php
@@ -340,6 +340,7 @@
 			foreach($adjusters as $adjuster => $label) {
 				foreach($stocks as $stock => $label) {
 					?>
+					
 					<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 						<url>
 							<loc>
