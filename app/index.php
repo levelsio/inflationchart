b7dfbb0a9deb1db2fbd2181cea27e1039c35c279
levelsio <7150848+levelsio@users.noreply.github.com>
2021-03-29 18:34:22 +0100

    x

--- app/index.php
+++ app/index.php
@@ -1111,9 +1111,9 @@
 		}
 		function removeEmojis(str) {
 			var ranges = [
-				'\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
-				'\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F
-				'\ud83d[\ude80-\udeff]'  // U+1F680 to U+1F6FF
+				'\ud83c[\udf00-\udfff]',
+				'\ud83d[\udc00-\ude4f]',
+				'\ud83d[\ude80-\udeff]' 
 			];
 			str = str.replace(new RegExp(ranges.join('|'), 'g'), '');
 			return str;
@@ -1208,23 +1208,23 @@
 			return n;
 			};
 			f = {
-			d: function() { // Day of month w/leading 0; 01..31
+			d: function() {
 				return _pad(f.j(), 2);
 			},
-			D: function() { // Shorthand day name; Mon...Sun
+			D: function() {
 				return f.l()
 				.slice(0, 3);
 			},
-			j: function() { // Day of month; 1..31
+			j: function() {
 				return jsdate.getDate();
 			},
-			l: function() { // Full day name; Monday...Sunday
+			l: function() {
 				return txt_words[f.w()] + 'day';
 			},
-			N: function() { // ISO-8601 day of week; 1[Mon]..7[Sun]
+			N: function() {
 				return f.w() || 7;
 			},
-			S: function() { // Ordinal suffix for day of month; st, nd, rd, th
+			S: function() {
 				var j = f.j();
 				var i = j % 10;
 				if (i <= 3 && parseInt((j % 100) / 10, 10) == 1) {
@@ -1232,115 +1232,115 @@
 				}
 				return ['st', 'nd', 'rd'][i - 1] || 'th';
 			},
-			w: function() { // Day of week; 0[Sun]..6[Sat]
+			w: function() {
 				return jsdate.getDay();
 			},
-			z: function() { // Day of year; 0..365
+			z: function() {
 				var a = new Date(f.Y(), f.n() - 1, f.j());
 				var b = new Date(f.Y(), 0, 1);
 				return Math.round((a - b) / 864e5);
 			},
 
-			W: function() { // ISO-8601 week number
+			W: function() {
 				var a = new Date(f.Y(), f.n() - 1, f.j() - f.N() + 3);
 				var b = new Date(a.getFullYear(), 0, 4);
 				return _pad(1 + Math.round((a - b) / 864e5 / 7), 2);
 			},
 
-			F: function() { // Full month name; January...December
+			F: function() {
 				return txt_words[6 + f.n()];
 			},
-			m: function() { // Month w/leading 0; 01...12
+			m: function() {
 				return _pad(f.n(), 2);
 			},
-			M: function() { // Shorthand month name; Jan...Dec
+			M: function() {
 				return f.F()
 				.slice(0, 3);
 			},
-			n: function() { // Month; 1...12
+			n: function() {
 				return jsdate.getMonth() + 1;
 			},
-			t: function() { // Days in month; 28...31
+			t: function() {
 				return (new Date(f.Y(), f.n(), 0))
 				.getDate();
 			},
 
-			L: function() { // Is leap year?; 0 or 1
+			L: function() {
 				var j = f.Y();
 				return j % 4 === 0 & j % 100 !== 0 | j % 400 === 0;
 			},
-			o: function() { // ISO-8601 year
+			o: function() {
 				var n = f.n();
 				var W = f.W();
 				var Y = f.Y();
 				return Y + (n === 12 && W < 9 ? 1 : n === 1 && W > 9 ? -1 : 0);
 			},
-			Y: function() { // Full year; event.g. 1980...2010
+			Y: function() {
 				return jsdate.getFullYear();
 			},
-			y: function() { // Last two digits of year; 00...99
+			y: function() {
 				return f.Y()
 				.toString()
 				.slice(-2);
 			},
 
-			a: function() { // am or pm
+			a: function() {
 				return jsdate.getHours() > 11 ? 'pm' : 'am';
 			},
-			A: function() { // AM or PM
+			A: function() {
 				return f.a()
 				.toUpperCase();
 			},
-			B: function() { // Swatch Internet time; 000..999
+			B: function() {
 				var H = jsdate.getUTCHours() * 36e2;
 				var i = jsdate.getUTCMinutes() * 60;
 				var s = jsdate.getUTCSeconds(); // Seconds
 				return _pad(Math.floor((H + i + s + 36e2) / 86.4) % 1e3, 3);
 			},
-			g: function() { // 12-Hours; 1..12
+			g: function() {
 				return f.G() % 12 || 12;
 			},
-			G: function() { // 24-Hours; 0..23
+			G: function() {
 				return jsdate.getHours();
 			},
-			h: function() { // 12-Hours w/leading 0; 01..12
+			h: function() {
 				return _pad(f.g(), 2);
 			},
-			H: function() { // 24-Hours w/leading 0; 00..23
+			H: function() {
 				return _pad(f.G(), 2);
 			},
-			i: function() { // Minutes w/leading 0; 00..59
+			i: function() {
 				return _pad(jsdate.getMinutes(), 2);
 			},
-			s: function() { // Seconds w/leading 0; 00..59
+			s: function() {
 				return _pad(jsdate.getSeconds(), 2);
 			},
-			u: function() { // Microseconds; 000000-999000
+			u: function() {
 				return _pad(jsdate.getMilliseconds() * 1000, 6);
 			},
 
-			e: function() { // Timezone identifier; event.g. Atlantic/Azores, ...
+			e: function() {
 				/*				return that.date_default_timezone_get();
 				 */
 				throw 'Not supported (see source code of date() for timezone on how to add support)';
 			},
-			I: function() { // DST observed?; 0 or 1
+			I: function() {
 				var a = new Date(f.Y(), 0);
 				var c = Date.UTC(f.Y(), 0);
 				var b = new Date(f.Y(), 6);
 				var d = Date.UTC(f.Y(), 6); // Jul 1 UTC
 				return ((a - c) !== (b - d)) ? 1 : 0;
 			},
-			O: function() { // Difference to GMT in hour format; event.g. +0200
+			O: function() {
 				var tzo = jsdate.getTimezoneOffset();
 				var a = Math.abs(tzo);
 				return (tzo > 0 ? '-' : '+') + _pad(Math.floor(a / 60) * 100 + a % 60, 4);
 			},
-			P: function() { // Difference to GMT w/colon; event.g. +02:00
+			P: function() {
 				var O = f.O();
 				return (O.substr(0, 3) + ':' + O.substr(3, 2));
 			},
-			T: function() { // Timezone abbreviation; event.g. EST, MDT, ...
+			T: function() {
 				/*				var abbr, i, os, _default;
 				if (!tal.length) {
 				tal = that.timezone_abbreviations_list();
@@ -1366,25 +1366,25 @@
 				*/
 				return 'UTC';
 			},
-			Z: function() { // Timezone offset in seconds (-43200...50400)
+			Z: function() {
 				return -jsdate.getTimezoneOffset() * 60;
 			},
 
-			c: function() { // ISO-8601 date.
+			c: function() {
 				return 'Y-m-d\\TH:i:sP'.replace(formatChr, formatChrCb);
 			},
-			r: function() { // RFC 2822
+			r: function() {
 				return 'D, d M Y H:i:s O'.replace(formatChr, formatChrCb);
 			},
-			U: function() { // Seconds since UNIX epoch
+			U: function() {
 				return jsdate / 1000 | 0;
 			}
 			};
 			this.date = function(format, timestamp) {
 			that = this;
-			jsdate = (timestamp === undefined ? new Date() : // Not provided
-				(timestamp instanceof Date) ? new Date(timestamp) : // JS Date()
-				new Date(timestamp * 1000) // UNIX timestamp (auto-convert to int)
+			jsdate = (timestamp === undefined ? new Date() :
+				(timestamp instanceof Date) ? new Date(timestamp) :
+				new Date(timestamp * 1000)
 			);
 			return format.replace(formatChr, formatChrCb);
 			};
@@ -1527,7 +1527,6 @@
 				function animateDividedByLine() {
 					is_animating=true;
 
-					// console.log('animateDividedByLine');
 					animationDataBufferIterator=0;
 					animationWhatAreWeAnimating='divided_by';
 
@@ -1890,7 +1889,6 @@
 				/* <new, show adjusted nominal values too as blue line> */
 					if(dataset.id==adjuster_selected+'_adjuster') {
 						dataset.hidden=false;
-						// dataset.borderColor='rgba(66, 165, 255)';
 						notTheStockOrTheAdjustedStock=false;
 					}
 				/* </new, show adjusted nominal values too as blue line> */
