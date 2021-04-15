985ffaae4f97b71efee69f384835feb1dc88bd6f
levelsio <7150848+levelsio@users.noreply.github.com>
2021-04-15 01:20:58 +0100

    x

--- app/index.php
+++ app/index.php
@@ -48,7 +48,7 @@
 		$query=$db->prepare("SELECT epoch FROM inflationchart WHERE epoch IS NOT NULL AND epoch IS NOT '' ORDER BY epoch DESC");
 		$query->execute();
 		$newestEpoch=$query->fetchAll(PDO::FETCH_ASSOC)[0]['epoch'];
-		if($newestEpoch<strtotime("-45 days")) { /* alert after 1.5mo of old data */
+		if($newestEpoch<strtotime("-70 days")) { /* alert after 1.5mo of old data */
 			sendToAdminTelegram("📈 InflationChart.com: source data is ".timeAgoLong($newestEpoch)." old, time to update maybe? Thanks!");
 		}
 	// </add reminder for stale data>
