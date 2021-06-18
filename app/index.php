1eae9ad08e6021f6cd960f49dada30826843cd54
levelsio <7150848+levelsio@users.noreply.github.com>
2021-06-18 22:26:47 +0700

    x

--- app/index.php
+++ app/index.php
@@ -2650,11 +2650,10 @@ function sendToAdminTelegram($message) {
 	global $config;
 	$telegram_bot_token=$config['telegramAdminChat']['bot_token'];
 	$telegram_chat_id=$config['telegramAdminChat']['chat_id'];
-	file_put_contents('/srv/lastAdminTelegramTimestamp.txt',time());
 	// file_get_contents('https://api.telegram.org/bot'.$telegram_bot_token.'/sendMessage?chat_id='.$telegram_chat_id.'&parse_mode=markdown&disable_web_page_preview=true&text='.urlencode($message).'&disable_web_page_preview=true');
 	// use shell exec to do it async and not slow down entire site
 	$text=$message;
-	$message='inflationchart '.' '.$_SERVER["SCRIPT_NAME"].' '.__FILE__.' '.$message;
+	file_put_contents('/srv/lastAdminTelegramTimestamp.txt',time()."\n".$message);
 	// shell_exec('curl '.escapeshellarg('https://api.telegram.org/bot'.$telegram_bot_token.'/sendMessage?chat_id='.$telegram_chat_id.'&text='.urlencode($text).'&parse_mode=markdown&disable_web_page_preview=true').' > /dev/null 1>/dev/null &');
 	shell_exec('curl '.escapeshellarg('https://api.telegram.org/bot'.$telegram_bot_token.'/sendMessage?chat_id='.$telegram_chat_id.'&text='.urlencode($text).'&parse_mode=markdown&disable_web_page_preview=true').' > /dev/null 1>/dev/null &');
 }
